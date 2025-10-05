17c214241621a793fac41bc403a8a3816c5bb2b1
Abhishek Bajpai <abhishekbajpai449@gmail.com>
2025-10-05 19:05:27 +0530

    init commit

---
+++ reactscript.js
@@ -0,0 +1,10 @@
+const heading  = React.createElement("h1", {
+    id:"headingId"
+}, "Hello World from React script file");
+
+
+const parentDiv = React.createElement("div", {id: "parentDiv"}, heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parentDiv);
